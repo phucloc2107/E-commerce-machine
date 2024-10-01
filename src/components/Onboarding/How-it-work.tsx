c377d9ab4f1beeeb5c944013c362baaf8d66b466
PhucLoc <longphucloc123@gmail.com>
2024-10-01 21:10:00 +0700

    feat: create layout How E-commerce work

---
+++ src/components/Onboarding/How-it-work.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const HowEcommerceWork = () => {
+    const [tabs, setTabs] = useState('users');
+
+    return (
+        <div className="w-full flex flex-col justify-center items-center">
+            <h1 className="text-5xl font-medium">How E-commerce Work</h1>
+
+            {/* Tab Headers */}
+            <div className="grid min-w-[800px] grid-cols-3 text-center my-7">
+                <h2
+                    className={`${tabs === "users" ? "p2-medium border-b-4 border-b-yellow-400" : "p2 border-b border-yellow-200 text-gray-400"} h-9 cursor-pointer md:h-12 text-xl`}
+                    onClick={() => setTabs('users')}
+                >
+                    For users
+                </h2>
+
+                <h2
+                    className={`${tabs === "sellers" ? "p2-medium border-b-4 border-b-yellow-400" : "p2 border-b border-yellow-200 text-gray-400"} h-9 cursor-pointer md:h-12 text-xl`}
+                    onClick={() => setTabs('sellers')}
+                >
+                    For sellers
+                </h2>
+
+                <h2
+                    className={`${tabs === "admin" ? "p2-medium border-b-4 border-b-yellow-400" : "p2 border-b border-yellow-200 text-gray-400"} h-9 cursor-pointer md:h-12 text-xl`}
+                    onClick={() => setTabs('admin')}
+                >
+                    For administrators
+                </h2>
+            </div>
+
+            {/* Tab Content */}
+            <div className="w-2/3 h-[400px] text-center">
+                {tabs === 'users' && (
+                    <div className="flex w-full h-full">
+                        <img src="../../../public/users.png" className="w-1/2 h-1/2 rounded-lg"/>
+                        <div className="flex flex-col text-left w-1/2 ml-24">
+                            <h1 className="text-3xl font-medium">For users to use E-commerce</h1>
+                            <p className="text-base mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                )}
+
+                {tabs === 'sellers' && (
+                    <div className="flex justify-between w-full h-full">
+                        <img src="../../../public/sellers.png" className="w-1/2 h-1/2 rounded-lg"/>
+                        <div className="flex flex-col text-left w-1/2 ml-24">
+                            <h1 className="text-3xl font-medium">For seller to business E-commerce</h1>
+                            <p className="text-base mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>                    
+                    </div>
+                )}
+
+                {tabs === 'admin' && (
+                    <div className="flex justify-between w-full h-full">
+                        <img src="../../../public/admin.png" className="w-1/2 h-1/2 rounded-lg"/>
+                        <div className="flex flex-col text-left w-1/2 ml-24">
+                            <h1 className="text-3xl font-medium">For admin to manager E-commerce</h1>
+                            <p className="text-base mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default HowEcommerceWork;
--- src/components/Onboarding/index.tsx
+++ src/components/Onboarding/index.tsx
@@ -1,11 +1,26 @@
-import Header from "../Header";
+import { Button } from "antd";
+import Header from "./Header";
+import HowEcommerceWork from "./How-it-work";
 
 const Onboarding = () => {
     return(
         <div className="w-screen h-screen">
             <Header />
-            <h1 className="text-green-400 text-7xl">Hello World!</h1>
-            <p className="text-red-400 text-5xl">Let started</p>
+            
+            <div className="w-full h-[686px] flex justify-center items-center">
+                <div className="w-2/3 h-1/2 flex flex-row justify-between">
+                    <div className="w-1/2 h-full">
+                        <h1 className="text-5xl font-medium">Provide a platform for buying, selling and trading products</h1>
+                        <h1 className="text-5xl text-yellow-400 font-medium">convenient and fast</h1>
+                        <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <Button className="text-center px-40 py-6 bg-black text-white mt-8" size="large">Get Started</Button>
+                    </div>
+
+                    <img src="../../../public/picture.png"  className="h-full w-[45%]"/>
+                </div>
+            </div>
+
+            <HowEcommerceWork />
         </div>
     )
 }
